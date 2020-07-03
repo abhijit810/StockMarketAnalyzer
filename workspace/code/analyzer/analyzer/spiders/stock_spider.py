468f27d5117669478682e91ad76f6159d1b57c70
Abhijit <abhijeet.patra8@gmail.com>
2020-07-03 21:49:10 +0530

    first customized scrape, learnt how to pick particular elements from the site, will start actual file construction next

--- workspace/code/analyzer/analyzer/spiders/stock_spider.py
+++ workspace/code/analyzer/analyzer/spiders/stock_spider.py
@@ -1,18 +1,32 @@
 import scrapy
+import os
+import datetime
+from scrapy.selector import Selector 
+from scrapy.http import HtmlResponse
 
 class StockSpider(scrapy.Spider):
     name = "stock"
-
+    urls = ['https://www.screener.in/company/TRENT/consolidated/']
     def start_requests(self):
-        urls = [
-            'https://www.screener.in/company/TRENT/consolidated/'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        for url in self.urls:
+            yield scrapy.Request(url=url, callback=self.parse_mine)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+        page = response.url.split("/")[-3]
+        today = datetime.datetime.now()
+        #filename = 'stock_'+str(today.year)+'_'+str(today.month)+'_'+str(today.day)+'_'+page+'.txt'
+        filename = 'stock_'+str(today.strftime('%Y_%h_%d'))+'_'+page+'.txt'
+        os.chdir('output')
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
+
+    def parse_mine(self,response):
+        page = response.url.split("/")[-3]
+        today = datetime.datetime.now()
+        #filename = 'stock_'+str(today.year)+'_'+str(today.month)+'_'+str(today.day)+'_'+page+'.txt'
+        filename = 'stock_'+str(today.strftime('%Y_%h_%d'))+'_'+page+'.csv'
+        os.chdir('output')
+        with open(filename, 'w') as f:
+            f.write(response.css('div#content-area div#company-info h1::text').get())
+        #for post in response.css(div@con)
