56feb4953283bc442a8fb1d907ef6c9ad9a719c0
Abhijit <abhijeet.patra8@gmail.com>
2020-08-08 21:48:34 +0530

    using mechanicalsoup instead of selenium

--- workspace/code/analyzer/readDatasets.py
+++ workspace/code/analyzer/readDatasets.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import time
 import re
-import mechanicalsoup
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
@@ -12,6 +12,7 @@ from robobrowser import RoboBrowser
 import requests
 from bs4 import BeautifulSoup
 import urllib
+import mechanicalsoup
 #from selenium.webdriver.chrome.options import Options
 
 os.chdir('file_processing')
@@ -28,15 +29,21 @@ def def_companies_df():
     return companies_df[['NAME OF COMPANY','Industry','Group']]
 
 
-def getUrl():
-    source_Url = 'https://www.screener.in/'
+def getMoneyControlUrl(companyName):
+    source_Url = 'http://www.moneycontrol.com/stocks/cptmarket/compsearchnew.php?search_data=&cid=&mbsearch_str=&topsearch_type=1&search_str=trent'
     mbrowser = mechanicalsoup.StatefulBrowser(user_agent='MechanicalSoup')
     mbrowser.open(source_Url)
-    mbrowser.select_form('#top-nav-search')
-    #browser.select_form('form[action="/search"]')
-    mbrowser.get_current_form().print_summary()
-    #browser['Search Company'] = "PCPL"
-    #browser.submit_selected()
+    mbrowser.select_form()
+    mbrowser['search_str'] = companyName
+    mbrowser.submit_selected()
+    form_list = str(mbrowser.get_current_page())
+    if form_list.find("Sorry, there are no matches for your search") != -1 : return None
+    if form_list.find("Search results") != -1 : 
+        links = mbrowser.get_current_page().findAll('a', attrs={'href': re.compile("^http://www.moneycontrol.com/")})
+        for link in links :
+            if '/'+companyName.lower()+'/' in link.get('href'):
+                return link.get('href')
+    return mbrowser.get_url()
 
 
 def getScreenerUrlSelenium(companyName = "None"):
@@ -46,7 +53,7 @@ def getScreenerUrlSelenium(companyName = "None"):
     chrome_options.add_experimental_option("useAutomationExtension", False)
     chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
     browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-    source_Url = 'https://www.screener.in/'
+    source_Url = 'http://www.moneycontrol.com/stocks/cptmarket/compsearchnew.php?search_data=&cid=&mbsearch_str=&topsearch_type=1&search_str=trent'
     browser.get(source_Url)
     browser.find_element_by_xpath(""" //*[@id="content-area"]/div/div/div/div/input """).send_keys("",companyName)
     browser.find_element_by_xpath(""" //*[@id="content-area"]/div/div/div/div/input """).send_keys(Keys.ENTER)
@@ -99,4 +106,17 @@ def usingurllib():
     soup = BeautifulSoup(fp)
     print(soup)
 
-print(getScreenerUrlSelenium("PCPL"))
+def getMoneycontrolUrlSelenium(companyName = "None"):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
+    browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    source_Url = 'https://www.moneycontrol.com/'
+    browser.get(source_Url)
+    browser.find_element_by_xpath(""" //*[@id="search_str"] """).send_keys("",companyName)
+    browser.find_element_by_xpath(""" //*[@id="autosuggestlist"]/ul/div/div/li[1]/a """).click()
+    return browser.current_url
+
+print(getMoneyControlUrl('TRENT'))
